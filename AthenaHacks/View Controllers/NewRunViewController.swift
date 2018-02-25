3e1b2cd51034c01415b0973c7f61aa9e494b5a29
Allison Reiss <allisonreiss1@gmail.com>
2018-02-25 02:04:35 -0800

    Run simulation gets data

--- AthenaHacks/View Controllers/NewRunViewController.swift
+++ AthenaHacks/View Controllers/NewRunViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewRunViewController: UIViewController {
 
@@ -18,13 +19,23 @@ class NewRunViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var paceLabel: UILabel!
     
-    
     private var run: Run?
+    private let locationManager = LocationManager.shared
+    private var seconds = 0
+    private var timer: Timer?
+    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    private var locationList: [CLLocation] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dataStackView.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+        locationManager.stopUpdatingLocation()
     }
 
     private func startRun() {
@@ -32,6 +43,15 @@ class NewRunViewController: UIViewController {
         dataStackView.isHidden = false
         startButton.isHidden = true
         stopButton.isHidden = false
+        
+        seconds = 0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+        locationList.removeAll()
+        updateDisplay()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            self.eachSecond()
+        }
+        startLocationUpdates()
     }
     
     private func stopRun() {
@@ -39,6 +59,8 @@ class NewRunViewController: UIViewController {
         dataStackView.isHidden = true
         startButton.isHidden = false
         stopButton.isHidden = true
+        
+        locationManager.stopUpdatingLocation()
     }
     
     @IBAction func startTapped(_ sender: Any) {
@@ -52,6 +74,7 @@ class NewRunViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
             self.stopRun()
+            self.saveRun()
             self.performSegue(withIdentifier: "RunDetailSegue", sender: nil)
         })
         alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
@@ -62,6 +85,48 @@ class NewRunViewController: UIViewController {
         present(alertController, animated: true)
     }
     
+    private func saveRun() {
+        let newRun = Run(context: CoreDataStack.context)
+        newRun.distance = distance.value
+        newRun.duration = Int16(seconds)
+        newRun.timestamp = Date()
+        
+        for location in locationList {
+            let locationObject = Location(context: CoreDataStack.context)
+            locationObject.timestamp = location.timestamp
+            locationObject.latitude = location.coordinate.latitude
+            locationObject.longitude = location.coordinate.longitude
+            newRun.addToLocations(locationObject)
+        }
+        
+        CoreDataStack.saveContext()
+        
+        run = newRun
+    }
+    
+    func eachSecond() {
+        seconds += 1
+        updateDisplay()
+    }
+    
+    private func updateDisplay() {
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedTime = FormatDisplay.time(seconds)
+        let formattedPace = FormatDisplay.pace(distance: distance,
+                                               seconds: seconds,
+                                               outputUnit: UnitSpeed.minutesPerMile)
+        
+        distanceLabel.text = "Distance:  \(formattedDistance)"
+        timeLabel.text = "Time:  \(formattedTime)"
+        paceLabel.text = "Pace:  \(formattedPace)"
+    }
+    
+    private func startLocationUpdates() {
+        locationManager.delegate = self
+        locationManager.activityType = .fitness
+        locationManager.distanceFilter = 10
+        locationManager.startUpdatingLocation()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -74,19 +139,20 @@ class NewRunViewController: UIViewController {
     }
 }
 
-/*
-extension NewRunViewController: SegueHandlerType {
-    enum SegueIdentifier: String {
-        case details = "RunDetailsViewController"
-    }
+extension NewRunViewController: CLLocationManagerDelegate {
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        switch segueIdentifier(for: segue) {
-        case .details:
-            let destination = segue.destination as! RunDetailsViewController
-            destination.run = run
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for newLocation in locations {
+            let howRecent = newLocation.timestamp.timeIntervalSinceNow
+            guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+            
+            if let lastLocation = locationList.last {
+                let delta = newLocation.distance(from: lastLocation)
+                distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+            }
+            
+            locationList.append(newLocation)
         }
     }
 }
- */
 
