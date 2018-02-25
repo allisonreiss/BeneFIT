76ff27ef9676a05d921d22f1e1cad470584308d4
Allison Reiss <allisonreiss1@gmail.com>
2018-02-24 18:52:23 -0800

    google Maps

--- AthenaHacks/View Controllers/RunViewController.swift
+++ AthenaHacks/View Controllers/RunViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import HyperTrack
 
-class RunViewController: UIViewController {
+HyperTrack.initialize("pk_8ca75b3f33fc4fe80123815d77754fe4bbcc6c5a")
 
+class RunViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+//    var locationManager : CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+/*
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = 200
+        locationManager.requestWhenInUseAuthorization()
+*/
     }
-
+/*
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            mapView.setRegion(region, animated: false)
+        }
+    }
+*/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
