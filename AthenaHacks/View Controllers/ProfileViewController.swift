64bc16b2f04ac55846ab47e4d145c50c3536feb3
Sarah Gemperle <sarah@guest-wireless-upc-1607-10-120-117-247.usc.edu>
2018-02-25 10:49:07 -0800

    fixed segue back to change charity and tab bar icons

--- AthenaHacks/View Controllers/ProfileViewController.swift
+++ AthenaHacks/View Controllers/ProfileViewController.swift
@@ -27,8 +27,6 @@ class ProfileViewController: UIViewController {
         
         profileImageView.layer.cornerRadius = 30
         profileImageView.clipsToBounds = true
-        var image: UIImage = #imageLiteral(resourceName: "owl")
-        profileImageView =UIImage(cgImage: image.cgImage)
         
         changeCharityButton.layer.cornerRadius = 15
         changeCharityButton.clipsToBounds = true
@@ -52,15 +50,9 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onChangeCharity(_ sender: Any) {
+        performSegue(withIdentifier: "chooseNewCharity", sender: nil)
     }
-    */
+    
 
 }
