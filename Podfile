6357183c92961d1b74e5f88ef2009945af451100
Sarah Gemperle <sarah@guest-wireless-upc-1607-10-120-117-247.usc.edu>
2018-02-24 22:30:31 -0800

    removed BDBOAUTH

--- Podfile
+++ Podfile
@@ -8,7 +8,6 @@ target 'AthenaHacks' do
   # Pods for AthenaHacks
 #  pod 'HyperTrack'
   pod 'AFNetworking', '~> 3.0'
-  pod 'BDBOAuth1Manager', '2.0.0'
   pod 'HyperTrack'
 
     post_install do |installer|
--- Pods/Target Support Files/Pods-AthenaHacks/Pods-AthenaHacks-frameworks.sh
+++ Pods/Target Support Files/Pods-AthenaHacks/Pods-AthenaHacks-frameworks.sh
@@ -136,7 +136,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GzipSwift/Gzip.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HyperTrack/HyperTrack.framework"
@@ -145,7 +144,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GzipSwift/Gzip.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HyperTrack/HyperTrack.framework"
